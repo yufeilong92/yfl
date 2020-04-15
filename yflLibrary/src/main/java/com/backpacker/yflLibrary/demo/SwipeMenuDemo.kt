40de75a79394fdfaa33ccc49e2a9782ff5f3c927
931697478@qq.com <931697478@qq.com>
2020-04-15 17:11:41 +0800

    增加recyclerView 左右滑动

---
+++ yflLibrary/src/main/java/com/backpacker/yflLibrary/demo/SwipeMenuDemo.kt
@@ -0,0 +1,14 @@
+package com.backpacker.yflLibrary.demo
+
+/**
+ * @Author : YFL  is Creating a porject in My Application
+ * @Package com.backpacker.yflLibrary.demo
+ * @Email : yufeilong92@163.com
+ * @Time :2020/4/15 17:08
+ * @Purpose :
+ */
+class SwipeMenuDemo {
+    /**
+     * 使用请查看 https://github.com/yufeilong92/SwipeMenuRecyclerView/tree/master
+     */
+}
