20a5e26384739aa64e1737ad5a0771dff16abd70
931697478@qq.com <931697478@qq.com>
2020-05-20 10:51:41 +0800

    增加圆角工具类

---
+++ yflLibrary/src/main/java/com/backpacker/yflLibrary/view/LollipopFixedWebView.java
@@ -0,0 +1,42 @@
+package com.backpacker.yflLibrary.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+/**
+ * @Author : YFL  is Creating a porject in xiaole
+ * @Package com.backpacker.yflLibrary.view
+ * @Email : yufeilong92@163.com
+ * @Time :2020/3/13 11:32
+ * @Purpose :  WebView 5.x 系统下  Resources$NotFoundException异常处理
+ */
+public class LollipopFixedWebView extends WebView {
+    public LollipopFixedWebView(Context context) {
+        super(getFixedContext(context));
+    }
+
+    public LollipopFixedWebView(Context context, AttributeSet attrs) {
+        super(getFixedContext(context), attrs);
+    }
+
+    public LollipopFixedWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(getFixedContext(context), attrs, defStyleAttr);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public LollipopFixedWebView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(getFixedContext(context), attrs, defStyleAttr, defStyleRes);
+    }
+
+    public LollipopFixedWebView(Context context, AttributeSet attrs, int defStyleAttr, boolean privateBrowsing) {
+        super(getFixedContext(context), attrs, defStyleAttr, privateBrowsing);
+    }
+    public static Context getFixedContext(Context context) {
+        if (Build.VERSION.SDK_INT >= 21 && Build.VERSION.SDK_INT < 23) // Android Lollipop 5.0 & 5.1
+            return context.createConfigurationContext(new Configuration());
+        return context;
+    }
+}
