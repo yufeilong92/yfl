4992ffdbc13c45c0909a3b148e98480bd6a16523
931697478@qq.com <931697478@qq.com>
2020-05-22 10:33:11 +0800

    增加获取缓存大小

--- yflLibrary/src/main/java/com/backpacker/yflLibrary/kotlin/SystemUtil.kt
+++ yflLibrary/src/main/java/com/backpacker/yflLibrary/kotlin/SystemUtil.kt
@@ -20,6 +20,7 @@ import com.backpacker.yflLibrary.vo.Constants
 import android.util.DisplayMetrics
 import com.backpacker.yflLibrary.java.LocaUtil
 import java.io.File
+import java.math.BigDecimal
 
 
 /**
@@ -512,5 +513,76 @@ object SystemUtil {
 
         }
         return deletedFiles
+    }
+
+    /**
+     * 获取缓存大小
+     * @param context
+     * @return
+     * @throws Exception
+     */
+    @Throws(java.lang.Exception::class)
+    fun getTotalCacheSize(context: Context): String? {
+        var cacheSize= getFolderSize(context.cacheDir)
+        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+            cacheSize += getFolderSize(context.externalCacheDir!!)
+        }
+        return getFormatSize(cacheSize.toDouble())
+    }
+
+    // 获取文件大小
+    //Context.getExternalFilesDir() --> SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
+    //Context.getExternalCacheDir() --> SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
+    @Throws(java.lang.Exception::class)
+    fun getFolderSize(file: File): Long {
+        var size: Long = 0
+        try {
+            val fileList = file.listFiles()
+            for (i in fileList.indices) {
+                // 如果下面还有文件
+                size = if (fileList[i].isDirectory) {
+                    size + getFolderSize(fileList[i])
+                } else {
+                    size + fileList[i].length()
+                }
+            }
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+        return size
+    }
+
+    /**
+     * 格式化单位
+     * @param size
+     * @return
+     */
+    fun getFormatSize(size: Double): String? {
+        val kiloByte = size / 1024
+        if (kiloByte < 1) {
+//            return size + "Byte";
+            return "0K"
+        }
+        val megaByte = kiloByte / 1024
+        if (megaByte < 1) {
+            val result1 = BigDecimal.valueOf(kiloByte)
+            return result1.setScale(2, BigDecimal.ROUND_HALF_UP)
+                .toPlainString().toString() + "K"
+        }
+        val gigaByte = megaByte / 1024
+        if (gigaByte < 1) {
+            val result2 = BigDecimal.valueOf(megaByte)
+            return result2.setScale(2, BigDecimal.ROUND_HALF_UP)
+                .toPlainString().toString() + "M"
+        }
+        val teraBytes = gigaByte / 1024
+        if (teraBytes < 1) {
+            val result3 = BigDecimal.valueOf(gigaByte)
+            return result3.setScale(2, BigDecimal.ROUND_HALF_UP)
+                .toPlainString().toString() + "GB"
+        }
+        val result4 = BigDecimal.valueOf(teraBytes)
+        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString()
+            .toString() + "TB"
     }
 }
