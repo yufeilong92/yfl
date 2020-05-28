807ac8dc4a73ef029bc48a6e52fe4e5aa9b8d28a
931697478@qq.com <931697478@qq.com>
2020-05-28 17:17:20 +0800

    增加缓存

---
+++ yflLibrary/src/main/java/com/backpacker/yflLibrary/demo/WaveViewDemo.java
@@ -0,0 +1,40 @@
+package com.backpacker.yflLibrary.demo;
+
+import android.widget.FrameLayout;
+
+/**
+ * @Author : YFL  is Creating a porject in My Application
+ * @Package com.backpacker.yflLibrary.demo
+ * @Email : yufeilong92@163.com
+ * @Time :2020/5/28 17:16
+ * @Purpose :
+ */
+public class WaveViewDemo {
+/*      <FrameLayout
+    android:layout_width="match_parent"
+    android:layout_height="200dp"
+    android:background="#FF3366">
+
+        <com.liang.myapplication.WaveView3
+    android:id="@+id/waveView3"
+    android:layout_width="match_parent"
+    android:layout_height="15dp"
+    android:layout_gravity="bottom" />
+
+        <ImageView
+    android:id="@+id/imageView"
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:layout_gravity="bottom|center"
+    android:src="@drawable/logo" />
+
+    </FrameLayout>
+
+      val lp = FrameLayout.LayoutParams(-2, -2)
+        lp.gravity = BOTTOM or Gravity.CENTER
+        waveView3.setOnWaveAnimationListener {
+            lp.setMargins(0,0,0, (it+2).toInt())
+            imageView.layoutParams=lp
+        }
+    */
+}
