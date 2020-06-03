071d0308fe32f5d76276b6759c1e0c548efbe826
931697478@qq.com <931697478@qq.com>
2020-06-03 10:54:30 +0800

    提交通用dialog

---
+++ yflLibrary/src/main/java/com/backpacker/yflLibrary/view/GmDialog.kt
@@ -0,0 +1,41 @@
+package com.backpacker.yflLibrary.view
+
+import android.content.Context
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.WindowManager
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AlertDialog
+import com.example.UtilsLibrary.R
+
+/**
+ * @Author : YFL  is Creating a porject in My Application
+ * @Package com.backpacker.yflLibrary.kotlin
+ * @Email : yufeilong92@163.com
+ * @Time :2020/6/3 9:57
+ * @Purpose :对话框
+ */
+abstract class GmDialog(mContext: Context, @LayoutRes var  id:Int, var mGravity :Int ) : AlertDialog(mContext,R.style.dialog) {
+    private var metrics: DisplayMetrics = context.resources.displayMetrics
+
+    init {
+        window!!.setWindowAnimations(R.style.popup_animation)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSizeMode()
+        setCanceledOnTouchOutside(true)
+        setContentView(id)
+        initViewData()
+    }
+    abstract fun initViewData();
+
+    private fun setSizeMode() {
+        val params = window!!.attributes
+        params.width = metrics.widthPixels
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT
+        window!!.attributes = params
+        window!!.setGravity(mGravity)
+    }
+
+}
