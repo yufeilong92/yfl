2c7abf95afd4790ee33155ff93ae2346e109f0c7
931697478@qq.com <931697478@qq.com>
2020-07-20 14:56:36 +0800

    优化工具栏

--- yflLibrary/src/test/java/com/backpacker/yflLibrary/ExampleUnitTest.java
+++ yflLibrary/src/test/java/com/backpacker/yflLibrary/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.backpacker.yflLibrary;
 
+import com.backpacker.yflLibrary.kotlin.KotlinListUtil;
+
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +18,30 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void text() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        ArrayList<String> data = new ArrayList<>();
+        data.add("2");
+        data.add("4");
+  /*      KotlinListUtil.INSTANCE.mergeLists(list, data);
+        System.out.println(list);
+        ArrayList<Integer> ints = new ArrayList<>();
+        ints.add(1);
+        ints.add(1);
+        ints.add(3);
+        ints.add(14);
+        List<Integer> strings = KotlinListUtil.INSTANCE.goDistinctLists(ints);
+        System.out.println(strings);*/
+
+        System.out.println("并集"+KotlinListUtil.INSTANCE.unionList(list,data));
+        System.out.println("补集"+KotlinListUtil.INSTANCE.minusList(list,data));
+        System.out.println("交集"+KotlinListUtil.INSTANCE.intersectList(list,data));
+        System.out.println("差集"+KotlinListUtil.INSTANCE.subtractList(list,data));
     }
 }
