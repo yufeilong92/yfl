6092b922edb8dc18cfd3dc61361f7f81ad6dc157
yufeilong92 <931697478@qq.com>
2020-08-27 17:56:01 +0800

    优化自定义发送验证码

--- app/src/main/java/cn/yfl/myapplication/MainActivity2.kt
+++ app/src/main/java/cn/yfl/myapplication/MainActivity2.kt
@@ -20,7 +20,7 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         btn_send_code.setOnClickListener {
             KotlinCountDownTimer.startTime(
-                this, R.color.text_main_bg,     R.color.white, R.color.white, R.color.text_green, btn_send_code
+                this, R.color.text_main_bg,R.color.white, R.color.white, R.color.text_green, btn_send_code
             ) {
                 Toast.makeText(this, "结束", Toast.LENGTH_SHORT).show();
             }
