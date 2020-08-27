f520208f96d361d4feb23ea0aaa91624cb88a283
yufeilong92 <931697478@qq.com>
2020-08-27 15:16:36 +0800

    增加圆形图片

--- yflLibrary/src/main/java/com/backpacker/yflLibrary/view/customview/FlowLayout.java
+++ yflLibrary/src/main/java/com/backpacker/yflLibrary/view/customview/FlowLayout.java
@@ -8,7 +8,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.UtilsLibrary.R;
-
 import java.util.ArrayList;
 import java.util.List;
 /**
