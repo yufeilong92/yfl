3f1df5359e5ec49dcb41e9dfff42d6154a843ce3
931697478@qq.com <daxue920809>
2020-09-22 17:26:09 +0800

    提交数据接口

--- app/src/test/java/cn/yfl/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/cn/yfl/myapplication/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package cn.yfl.myapplication
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.random.Random
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +15,22 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun text() {
+        val list = mutableListOf<B>()
+        for (index in 0..50) {
+            val random = Math.random()*10
+            list.add(B(index, random.toInt() ));
+        }
+        list.sortWith(compareBy(
+            {it.age},{it.index}
+        ))
+        println(list)
+    }
+
+    inner class B(
+        val index: Int,
+        val age: Int
+    )
 }
